fc1994bbd17a02cc49620ea7bef114d263520835
Troy Tae <tjy970721@gmail.com>
2019-05-08 19:12:47 +0900

    support raised/unelevated

--- demos/general/src/index.tsx
+++ demos/general/src/index.tsx
@@ -4,6 +4,14 @@ import {MDCButton} from 'material-components-web-inferno';
 import '@material/button/mdc-button.scss';
 
 render(<div>
-  <MDCButton>Test!</MDCButton>
-  <MDCButton.Anchor href={'https://google.com'} target={'blank'}>Test!</MDCButton.Anchor>
+  <div>
+    <MDCButton>Test!</MDCButton>
+    <MDCButton raised={true}>Test!</MDCButton>
+    <MDCButton unelevated={true}>Test!</MDCButton>
+  </div>
+  <div>
+    <MDCButton.Anchor href={'https://google.com'} target={'blank'}>Test!</MDCButton.Anchor>
+    <MDCButton.Anchor raised={true} href={'https://google.com'} target={'blank'}>Test!</MDCButton.Anchor>
+    <MDCButton.Anchor unelevated={true} href={'https://google.com'} target={'blank'}>Test!</MDCButton.Anchor>
+  </div>
 </div>, document.getElementById('app'));
--- packages/mdc-button/index.tsx
+++ packages/mdc-button/index.tsx
@@ -1,14 +1,30 @@
+import classnames from 'classnames';
 import {Component, Fragment} from 'inferno';
 
 import {CSS_CLASSES} from './constant';
 
-interface MDCButtonProps {}
+interface MDCButtonProps {
+  raised?: boolean;
+  unelevated?: boolean;
+}
 
 abstract class AbstractMDCButton<A, E> extends Component<MDCButtonProps & DetailedHTMLProps<A, E>> {
-  internalRender(props) {
+  cssClasses(className) {
+    const {
+      raised,
+      unelevated,
+    } = this.props;
+
+    return classnames(className, CSS_CLASSES.ROOT, {
+      [CSS_CLASSES.RAISED]: raised,
+      [CSS_CLASSES.UNELEVATED]: unelevated,
+    });
+  }
+
+  internalRender() {
     const {
       children
-    } = props;
+    } = this.props;
 
     return <Fragment>
       <span className={CSS_CLASSES.LABEL}>{children}</span>
@@ -19,22 +35,24 @@ abstract class AbstractMDCButton<A, E> extends Component<MDCButtonProps & Detail
 export default class MDCButton extends AbstractMDCButton<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
   render() {
     const {
+      className,
       ...otherProps
     } = this.props;
 
-    return <button className={CSS_CLASSES.ROOT} {...otherProps}>
-      {this.internalRender(this.props)}
+    return <button className={this.cssClasses(className)} {...otherProps}>
+      {this.internalRender()}
     </button>;
   }
 
   static Anchor = class extends AbstractMDCButton<AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement> {
     render() {
       const {
+        className,
         ...otherProps
       } = this.props;
 
-      return <a className={CSS_CLASSES.ROOT} {...otherProps}>
-        {this.internalRender(this.props)}
+      return <a className={this.cssClasses(className)} {...otherProps}>
+        {this.internalRender()}
       </a>;
     }
   }
