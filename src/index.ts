5ecf13dc27eb654cb85c3255f31e1f727c7a5468
Maciek Grzybek <maciekgrzybek1@gmail.com>
2019-10-02 14:43:42 +0100

    Add toad pattern

--- src/index.ts
+++ src/index.ts
@@ -17,3 +17,12 @@ render('Blinker', new GameOfLifeEngine([
   [false, false, false, false, false],
   [false, false, false, false, false],
 ]));
+
+render('Toad', new GameOfLifeEngine([
+  [false, false, false, false, false, false],
+  [false, false, false, false, false, false],
+  [false, false, true, true, true, false],
+  [false, true, true, true, false, false],
+  [false, false, false, false, false, false],
+  [false, false, false, false, false, false],
+]));
