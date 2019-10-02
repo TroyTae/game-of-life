0972097bf0f5b7f3906f08435a0752810615f9f1
Maciek Grzybek <maciekgrzybek1@gmail.com>
2019-10-02 15:00:38 +0100

    Add extra spaces for readability

--- src/index.ts
+++ src/index.ts
@@ -20,9 +20,9 @@ render('Blinker', new GameOfLifeEngine([
 
 render('Lighthouse', new GameOfLifeEngine([
   [false, false, false, false, false, false],
-  [false, true, true, false, false, false],
-  [false, true, false, false, false, false],
-  [false, false, false, false, true, false],
-  [false, false, false, true, true, false],
+  [false, true,  true,  false, false, false],
+  [false, true,  false, false, false, false],
+  [false, false, false, false, true,  false],
+  [false, false, false, true,  true,  false],
   [false, false, false, false, false, false],
 ]));
