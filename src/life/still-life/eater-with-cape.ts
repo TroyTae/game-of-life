a78db2060903a8580b0bce6e3b44ed8f21fd6060
Akash Goyal <30392843+scavenger29@users.noreply.github.com>
2019-10-19 13:22:14 +0530

    Fixed indentation issues

--- src/life/still-life/eater-with-cape.ts
+++ src/life/still-life/eater-with-cape.ts
@@ -1,8 +1,8 @@
-export default {	
-  title: 'Eater with cape',	
-  life: [	
-    [0, 0, 0],	
-    [0, 1, 0],	
-    [0, 0, 0],	
-  ],	
+export default {
+  title: 'Eater with cape',
+  life: [
+    [0, 0, 0],
+    [0, 1, 0],
+    [0, 0, 0],
+  ],
 };
