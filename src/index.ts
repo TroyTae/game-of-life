78511bcf1b43202b286bf5ebc5b1bad9bbeccc8f
태재영 <tjy970721@gmail.com>
2020-03-06 20:55:34 +0900

    chore: 128P10.2

--- src/index.ts
+++ src/index.ts
@@ -695,6 +695,7 @@ import _110P62 from './life/oscillator/110P62';
 import _112P15 from './life/oscillator/112P15';
 import _117P18 from './life/oscillator/117P18';
 import _124P21 from './life/oscillator/124P21';
+import _128P10_2 from './life/oscillator/128P10_2';
 import AForAll from './life/oscillator/a-for-all';
 import AchimsOtherP16 from './life/oscillator/achims-other-p16';
 import AchimsP4 from './life/oscillator/achims-p4';
@@ -714,6 +715,7 @@ renderTitle('Oscillator', 1);
   _112P15,
   _117P18,
   _124P21,
+  _128P10_2,
   AForAll,
   AchimsOtherP16,
   AchimsP4,
