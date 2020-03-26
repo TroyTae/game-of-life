0ea392dde13494367c01f2ed5c78584eae36ad8b
태재영 <tjy970721@gmail.com>
2020-03-26 17:44:57 +0900

    chore: 22P2

--- src/index.ts
+++ src/index.ts
@@ -693,6 +693,7 @@ import _14P2_3 from './life/oscillator/14P2_3';
 import _14P2_4 from './life/oscillator/14P2_4';
 import _20P2 from './life/oscillator/20P2';
 import _21P2 from './life/oscillator/21P2';
+import _22P2 from './life/oscillator/22P2';
 import _101 from './life/oscillator/101';
 import _106P135 from './life/oscillator/106P135';
 import _110P62 from './life/oscillator/110P62';
@@ -724,6 +725,7 @@ renderTitle('Oscillator', 1);
   _14P2_4,
   _20P2,
   _21P2,
+  _22P2,
   _101,
   _106P135,
   _110P62,
