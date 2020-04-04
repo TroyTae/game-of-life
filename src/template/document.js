a9987e62b81d8230b1b76503f8b31f74be79f764
태재영 <tjy970721@gmail.com>
2020-04-04 20:57:15 +0900

    feat: temp web design

--- src/template/document.js
+++ src/template/document.js
@@ -10,6 +10,26 @@ module.exports = ({
         <base href="./">
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=5.0">
+        <style>
+          html {
+            font: 16px sans-serif;
+          }
+          body {
+            color: #FFF;
+            background: #000;
+            text-align: center;
+          }
+          a {
+            color: #2997ff;
+            text-decoration: none;
+          }
+          a:hover, a:active {
+            text-decoration: underline;
+          }
+          canvas {
+            border-radius: 0.25rem;
+          }
+        </style>
       </head>
       <body>
         ${bodyHTML}
