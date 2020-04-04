2355121b7aaa6ac4ac6e73829db29628ade1841d
태재영 <tjy970721@gmail.com>
2020-04-04 21:09:09 +0900

    feat: file-loader

--- src/template/document.js
+++ src/template/document.js
@@ -7,29 +7,9 @@ module.exports = ({
     <html lang="en">
       <head>
         <title>Conway's Game of Life${title ? ` - ${title}` : ''}</title>
-        <base href="./">
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=5.0">
-        <style>
-          html {
-            font: 16px sans-serif;
-          }
-          body {
-            color: #FFF;
-            background: #000;
-            text-align: center;
-          }
-          a {
-            color: #2997ff;
-            text-decoration: none;
-          }
-          a:hover, a:active {
-            text-decoration: underline;
-          }
-          canvas {
-            border-radius: 0.25rem;
-          }
-        </style>
+        <link rel="stylesheet" href="/${require('./style.css').default}">
       </head>
       <body>
         ${bodyHTML}
