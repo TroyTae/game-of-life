f5138d93fc24a42bffe1f9ec1239d7e2594621dc
태재영 <tjy970721@gmail.com>
2020-06-08 23:36:40 +0900

    refactor: change life file type

--- src/life/oscillator/billiard-table/29P9.ts
+++
@@ -1,8 +0,0 @@
-const title = `29P9`;
-const life = [
-  [0, 0, 0],
-  [0, 1, 0],
-  [0, 0, 0],
-];
-
-export {title, life};
--- src/life/still-life/strict-still-life/block.ts
+++
@@ -1,9 +0,0 @@
-const title = `Block`;
-const life = [
-  [0, 0, 0, 0],
-  [0, 1, 1, 0],
-  [0, 1, 1, 0],
-  [0, 0, 0, 0],
-];
-
-export {title, life};
