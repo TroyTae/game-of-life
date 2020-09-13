a96f77046dfb52e37b6e649428ca850aa0c5e64c
태재영 <tjy970721@gmail.com>
2020-09-13 17:19:35 +0900

    feat: post build

--- scripts/prebuild.js
+++ scripts/prebuild.js
@@ -45,28 +45,28 @@ const indexes = generateIndexes(2, directories);
 const lifes = indexes.filter((life) => !life.level);
 
 fs.writeFileSync(path.join('src', 'index.tsx'), `
-  import { h, render } from 'preact';
-  import Router from 'preact-router';
-  import { Link } from 'preact-router/match';
-  import Page from './page';
-  ${lifes
-    .map((life) => `
-  import ${life.name} from '${life.path}';`)
-    .join('')}
+import { h, render } from 'preact';
+import Router from 'preact-router';
+import { Link } from 'preact-router/match';
+import Page from './page';
+${lifes
+  .map((life) => `
+import ${life.name} from '${life.path}';`)
+  .join('')}
 
-  render((
-    <Router>
-      <article path='/'>
-        <h1>Conway's Game of Life</h1>
-        ${indexes.map((life) => life.level ? `
-        <h${life.level}>${life.name}</h${life.level}>` : `
-        <nav><Link href='${life.href}'>{${life.name}.title}</Link></nav>`
-        ).join('')}
-      </article>
-      ${lifes
-        .map((life) => `
-      <Page path='${life.href}' lifeData={${life.name}} />`)
-        .join('')}
-    </Router>
-  ), document.getElementById('app'));
+render((
+  <Router>
+    <article path='/'>
+      <h1>Conway's Game of Life</h1>
+      ${indexes.map((life) => life.level ? `
+      <h${life.level}>${life.name}</h${life.level}>` : `
+      <nav><Link href='${life.href}'>{${life.name}.title}</Link></nav>`
+      ).join('')}
+    </article>
+    ${lifes
+      .map((life) => `
+    <Page path='${life.href}' lifeData={${life.name}} />`)
+      .join('')}
+  </Router>
+), document.getElementById('app'));
 `);
