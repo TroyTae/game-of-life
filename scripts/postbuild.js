5e2b7ff270927aca45d499979823d3925bde9dcf
태재영 <tjy970721@gmail.com>
2020-09-13 17:59:12 +0900

    feat: prebuild

--- scripts/postbuild.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs-extra');
-const path = require('path');
-const distDir = 'dist';
-fs.emptyDirSync(distDir);
-fs.copyFileSync(
-  path.join('src', 'static', 'robots.txt'),
-  path.join(distDir, 'robots.txt')
-);
