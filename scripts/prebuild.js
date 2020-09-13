6a3b43479767c4c31cb34766df7ab7ff04fac1c5
태재영 <tjy970721@gmail.com>
2020-09-13 15:57:07 +0900

    prebuild index

--- scripts/prebuild.js
+++ scripts/prebuild.js
@@ -1,32 +1,47 @@
 const fs = require('fs-extra');
 const path = require('path');
-const mkdirp = require('mkdirp');
 const { readDirectory } = require('troyjs/node');
 
-const lifePath = path.join('src', 'life');
-const buildPath = path.join('src', 'build');
-
-function prebuild(dataList) {
-  dataList.forEach((data) => {
-    if (Array.isArray(data.children)) {
-      prebuild(data.children);
+const directories =  readDirectory(path.join('src', 'life'));
+const renderLife = (level, directories) => {
+  return directories.reduce((arr, obj) => {
+    if (Array.isArray(obj.children)) {
+      arr.push({ level, name: obj.name });
+      renderLife(level + 1, obj.children)
+        .forEach((v) => arr.push(v));
     } else {
-      const jsonPath = path.join('..', data.path);
-      const tsPath = data.path.replace(path.join(lifePath), buildPath).replace('.json', '.ts');
-      if (
-        !fs.existsSync(tsPath) ||
-        fs.statSync(data.path).mtime > fs.statSync(tsPath).mtime
-      ) {
-        const pattern = require(jsonPath);
-        mkdirp.sync(path.dirname(tsPath));
-        fs.writeFileSync(tsPath, `
-          import {renderLife} from 'renderer';
-          export const title = "${pattern.title}";
-          renderLife([${pattern.life.map((arr) => `[${arr.join()}]`).join()}]);
-        `);
-      }
+      arr.push({
+        ...obj,
+        href: '/' + obj.path
+          .split(path.sep)
+          .slice(2)
+          .join('/')
+      });
     }
-  });
-}
+    return arr;
+  }, []);
+};
+const lifes = renderLife(2, directories);
 
-prebuild(readDirectory(lifePath));
+fs.writeFileSync(path.join('src', 'index.tsx'), `
+  import { h, render } from 'preact';
+  import Router from 'preact-router';
+  import { Link } from 'preact-router/match';
+
+  render((
+    <Router>
+      <article path='/'>
+        <h1>Conway's Game of Life</h1>
+        ${lifes.map((life) => life.level ? `
+        <h${life.level}>${life.name}</h${life.level}>` : `
+        <nav><Link href='${life.href}'>${life.name}</Link></nav>`
+        ).join('')}
+      </article>
+      ${lifes
+        .filter((life) => !life.level)
+        .map((life) => `
+      <h1 path='${life.href}'>${JSON.stringify(life)}</h1>`)
+        .join('')}
+    </Router>
+  ), document.getElementById('app'));
+`);
