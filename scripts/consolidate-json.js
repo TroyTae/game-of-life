ddf7d51bc07d7b8630ff88bf79c867fbe145a0ee
Andydevs <akanshul97@gmail.com>
2021-02-28 19:04:47 -0500

    Created consolidate-json.js script

---
+++ scripts/consolidate-json.js
@@ -0,0 +1,67 @@
+/**
+ * Script used to consolodate life 
+ * directory files into one json file
+ */
+const fs = require('fs')
+const path = require('path')
+
+// Filepath of life.json files
+const LIFE_DIRECTORY = path.resolve(__dirname, '..', 'life')
+
+/**
+ * Recursively walk directory and look for files. Do something 
+ * with each of them.
+ * 
+ * @param {string} dir directory to check
+ * @param {Function<string>} callback called on every file found
+ */
+function walk(dir, callback) {
+    let subpaths = fs.readdirSync(dir)
+    for (const subpath of subpaths) {
+        let fullsubpath = path.resolve(dir, subpath)
+        let stat = fs.statSync(fullsubpath)
+        if (stat.isDirectory()) {
+            walk(fullsubpath, callback)
+        }
+        else {
+            callback(fullsubpath)
+        }
+    }
+}
+
+/**
+ * Transform a folder name with dashes into
+ * a proper name with capitalized words and
+ * spaces
+ * 
+ * @param {string} name name of path folder
+ */
+let transform = name =>
+    name.split('-')
+        .map(word => word[0].toUpperCase() + word.slice(1))
+        .join(' ')
+
+// Read all json files in life folder and append them to this json document
+let json = []
+walk(LIFE_DIRECTORY, fpath => {
+    // Construct "category" and "subcategory" entries
+    // from folder/subfolder of the file. If the file 
+    // is not in a subfolder, the subcategory is the 
+    // same as the category
+    let relpath = path.relative(LIFE_DIRECTORY, fpath)
+    let parts = relpath.split(path.sep)
+    let category = parts[0]
+    let subcategory = parts[parts.length > 2 ? 1 : 0]
+    category = transform(category)
+    subcategory = transform(subcategory)
+
+    // Read data, add extra entries, and append to json array
+    let data = fs.readFileSync(fpath)
+    data = JSON.parse(data)
+    data.category = category
+    data.subcategory = subcategory
+    json.push(data)
+})
+
+// Write the json into life.json
+fs.writeFileSync('./src/life.json', JSON.stringify(json))
