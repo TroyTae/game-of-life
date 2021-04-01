16f834106d711cfc74c2baac3ae0be5d71aa77fe
태재영 <tjy970721@gmail.com>
2021-04-01 15:43:47 +0900

    feat: pattern datalist

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,29 @@
 import { GameOfLifeEngine } from './engine';
-import { createElement, APPEND, CANVAS } from 'noliter';
+import { createElement, APPEND, CANVAS, LABEL, INPUT, DATALIST, OPTION, MAIN } from 'noliter';
 import patterns from './patterns';
 
+const DATALIST_ID = 'patterns';
+
 document.body[APPEND](
-  createElement(CANVAS, (cvs) => {
-    const engine = new GameOfLifeEngine(cvs, patterns[0].life);
-    engine.startLife();
+  createElement(MAIN, (main) => {
+    main[APPEND](
+      createElement(DATALIST, (datalist) => {
+        datalist.id = DATALIST_ID;
+        patterns.forEach(({ title }) =>
+          datalist[APPEND](
+            createElement(OPTION, (option) => {
+              option.value = title;
+            })
+          )
+        );
+      }),
+      createElement(INPUT, (input) => {
+        input.setAttribute('list', DATALIST_ID);
+      })
+    );
   })
+  // createElement(CANVAS, (cvs) => {
+  //   const engine = new GameOfLifeEngine(cvs, patterns[0].life);
+  //   engine.startLife();
+  // })
 );
